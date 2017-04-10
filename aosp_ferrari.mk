ec6016d96e993beac36e0d7b1c2ca45551a065b6
Rishabh <rishabhad93@gmail.com>
2017-04-10 23:52:50 +0530

    Update aosp_ferrari.mk

--- aosp_ferrari.mk
+++ aosp_ferrari.mk
@@ -1,5 +1,6 @@
-# Copyright (C) 2014 The CyanogenMod Project
-#           (C) 2017 The LineageOS Project
+#
+# Copyright 2016 The CyanogenMod Project
+# Copyright 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,24 +13,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 $(call inherit-product, device/xiaomi/ferrari/full_ferrari.mk)
 
-<<<<<<< HEAD:aosp_ferrari.mk
-# Inherit some common LineageOS stuff.
+# Inherit some common aosp stuff.
 $(call inherit-product, vendor/aosp/common.mk)
 
->>>>>>> 9c4f891c5eed7ea299eafb125301a826ec2413c2:aosp_ferrari.mk
-# Must define platform variant before including any common things
-TARGET_BOARD_PLATFORM_VARIANT := msm8939
-
-# Device identifier. This must come after all inclusions
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := ferrari
 PRODUCT_NAME := aosp_ferrari
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 4i
 PRODUCT_MANUFACTURER := Xiaomi
-DEFAULT_ROOT_METHOD := supersu
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_MODEL := MI4i
+TARGET_VENDOR := Xiaomi
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.qualcomm.bt.hci_transport
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
