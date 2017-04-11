d07a6bbbeaf7ceb80bce5267860f976129d0e5eb
Rishabh <rishabhad93@gmail.com>
2017-04-11 21:32:35 +0530

    Update aosp_ferrari.mk

--- aosp_ferrari.mk
+++ aosp_ferrari.mk
@@ -1,32 +1,16 @@
-#
-# Copyright 2016 The CyanogenMod Project
-# Copyright 2017 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-$(call inherit-product, device/xiaomi/ferrari/full_ferrari.mk)
-
-# Inherit some common aosp stuff.
+# Inherit common AOSP-CAF stuff
 $(call inherit-product, vendor/aosp/common.mk)
 
-# Set those variables here to overwrite the inherited values.
-BOARD_VENDOR := Xiaomi
-PRODUCT_BRAND := Xiaomi
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/ferrari/full_ferrari.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT :=1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Device identifications
 PRODUCT_DEVICE := ferrari
 PRODUCT_NAME := aosp_ferrari
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := MI4i
-TARGET_VENDOR := Xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_BRAND := xiaomi
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_MODEL := xiaomi MI4i
