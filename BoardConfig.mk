61b2ce31ee1b68ef28f18023e7bd5d56613102fd
Rishabh <rishabhad93@gmail.com>
2017-04-30 17:56:23 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,5 +4,4 @@ include device/xiaomi/ferrari/board/*.mk
 
 TARGET_BOARD_INFO_FILE := device/xiaomi/ferrari/board-info.txt
 
-# inherit from the proprietary version
 -include vendor/xiaomi/ferrari/BoardConfigVendor.mk
